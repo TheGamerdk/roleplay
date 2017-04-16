163f032681ae779000054d9be09f40a7caa4b951
TheGamerdk <carlgustav03@gmail.com>
2017-04-16 15:45:13 +0200

    Added some post-admin password files

--- computer/bin/admin_pass.bat
+++ computer/bin/admin_pass.bat
@@ -3,7 +3,10 @@ SET /P variable=Password :
 if %variable% == j72HRG goto success
 if not %variable% == j72HRG goto fail
 :success
-echo yahh
+echo d | xcopy /s /e "%~dp0../data/locked/files/admin/emails" "%~dp0../filesystem/documents/emails"
+echo d | xcopy /s /e "%~dp0../data/locked/files/admin/work" "%~dp0../filesystem/documents/work"
+echo d | xcopy /s /e "%~dp0../data/locked/files/admin/private" "%~dp0../filesystem/documents/private"
+echo d | xcopy /s /e "%~dp0../data/locked/files/admin/bin" "%~dp0../bin"
 goto end
 :fail
 cls
